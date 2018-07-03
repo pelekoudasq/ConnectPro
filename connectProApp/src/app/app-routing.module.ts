f7bee004ac1ef53791bd82469fc85bf79c94c074
Ioannis Pelekoudas <peleioannis@msn.com>
2018-07-03 14:07:13 +0300

    login

--- connectProApp/src/app/app-routing.module.ts
+++ connectProApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: '',
+        component: WelcomePageComponent
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent
+    }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- connectProApp/src/app/app.module.ts
+++ connectProApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
