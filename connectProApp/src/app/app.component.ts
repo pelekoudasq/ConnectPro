f1d9e26ca57755fa2fd185f40a201a47b7b2955a
Ioannis Pelekoudas <peleioannis@msn.com>
2018-07-03 11:36:28 +0300

    comments

--- connectProApp/src/app/app.component.ts
+++ connectProApp/src/app/app.component.ts
@@ -1,10 +1,20 @@
+//imports
 import { Component } from '@angular/core';
 
+//component decorator, specify
 @Component({
   selector: 'app-root',
+  //define where html template located
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//all the logic is
 export class AppComponent {
   title = 'app';
 }
+
+//new Component
+//cmd: ng generate component $name
+//or: ng g c $name
+//
