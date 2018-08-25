a23d4a6708cb5e80f6399ebef5dd684241ec6dda
SofiaKstk <sofkontis@gmail.com>
2018-08-25 17:19:01 +0300

    exclude login req from token auth

--- connectProApp/src/app/jwt-interceptor.ts
+++ connectProApp/src/app/jwt-interceptor.ts
@@ -6,6 +6,12 @@ import { Observable } from 'rxjs';
 export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("intercepted request ... ");
+        //exclude login req from token auth
+        /*if (request.url === "http://localhost:3000/api/login"){
+            console.log("login..");
+            return;
+        }
+        console.log("not login");*/
         // add authorization header with jwt token if available
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         if (currentUser && currentUser.token) {
