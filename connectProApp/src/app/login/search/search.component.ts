b421ea1f5eec4152cb623a08a3978388269fe0c9
Ioannis Pelekoudas <peleioannis@msn.com>
2018-08-30 11:08:15 +0300

    tab fix

--- connectProApp/src/app/login/search/search.component.ts
+++ connectProApp/src/app/login/search/search.component.ts
@@ -4,23 +4,23 @@ import { AppRoutingModule } from '../../app-routing.module';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
 
-  currentUser: User;
-  users: User[] = [];
+    currentUser: User;
+    users: User[] = [];
 
-  constructor(private router: Router) {
-      this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-      if (!this.currentUser){
-          this.router.navigate(['']);
-      }
-  }
+    constructor(private router: Router) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+            if (!this.currentUser){
+                this.router.navigate(['']);
+            }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
